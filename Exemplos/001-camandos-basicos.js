3e7d7a52e89a9d7346e0461a89779e1c403fde11
CasarottiEduardo <124743631+CasarottiEduardo@users.noreply.github.com>
2023-02-14 21:19:37 -0300

    Aula Variévis

---
+++ Exemplos/001-camandos-basicos.js
@@ -0,0 +1,29 @@
+console.log("hello world!");
+
+let nome = 'Eduardo' ;
+let idade = 25 ;
+let altura = 1.75 ;
+let trabalha = true ;
+let corFavorita = 'laranja';
+let cores_favoritas =['laranja', 'roxo', 'verde', 'azul'];
+let endereco = {
+    'logradouro'    : 'Rua',
+    'nome_log'      : 'Brasil',
+    'n'             : '102 A ',
+    'bairro'        : 'centro',
+};
+
+let covid;
+let dengue = null;
+const exfunc = () => 10 ;
+
+console.log( 'Variavel nome = ' + typeof(nome));
+console.log( 'Variavel idade = ' + typeof(idade));
+console.log( 'Variavel altura = ' + typeof(altura));
+console.log( 'Variavel trabalha = ' + typeof(trabalha));
+console.log( 'Variavel corFavorita = ' + typeof(corFavorita));
+console.log( 'Variavel cores_favoritas = ' + typeof(cores_favoritas));
+console.log( 'Variavel endereco = ' + typeof(endereco));
+console.log( 'Variavel covid = ' + typeof(covid));
+console.log( 'Variavel dengue = ' + typeof(dengue));
+console.log( 'Variavel exfunc = ' + typeof(exfunc));
